aa4a0a7596849028c5481a5a13eaa5fa6ab0df48
minyoung.lim <minyoung.lim@rubycuve.com>
2021-03-07 03:01:54 +0900

    week2-8

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -3,15 +3,17 @@ import { produce } from "immer";
 
 import { setCookie, getCookie, deleteCookie } from "../../shared/Cookie";
 
+import { auth } from "../../shared/firebase";
+
 // actions
-const LOG_IN = "LOG_IN";
 const LOG_OUT = "LOG_OUT";
 const GET_USER = "GET_USER";
+const SET_USER = "SET_USER";
 
 // action creators
-const logIn = createAction(LOG_IN, (user) => ({ user }));
 const logOut = createAction(LOG_OUT, (user) => ({ user }));
 const getUser = createAction(GET_USER, (user) => ({ user }));
+const setUser = createAction(SET_USER, (user) => ({ user }));
 
 // initialState
 const initialState = {
@@ -19,31 +21,62 @@ const initialState = {
   is_login: false,
 };
 
-
 // middleware actions
 const loginAction = (user) => {
-    return function (dispatch, getState, {history}){
-        console.log(history);
-        dispatch(logIn(user));
-        history.push('/');
-    }
-}
+  return function (dispatch, getState, { history }) {
+    console.log(history);
+    dispatch(setUser(user));
+    history.push("/");
+  };
+};
 
+const signupFB = (id, pwd, user_name) => {
+  return function (dispatch, getState, {history}){
+
+    auth
+      .createUserWithEmailAndPassword(id, pwd)
+      .then((user) => {
+
+        console.log(user);
+        
+        auth.currentUser.updateProfile({
+          displayName: user_name,
+        }).then(()=>{
+          dispatch(setUser({user_name: user_name, id: id, user_profile: ''}));
+          history.push('/');
+        }).catch((error) => {
+          console.log(error);
+        });
+
+        // Signed in
+        // ...
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        console.log(errorCode, errorMessage);
+        // ..
+      });
+
+  }
+}
 
 // reducer
 export default handleActions(
   {
-    [LOG_IN]: (state, action) =>
+    [SET_USER]: (state, action) =>
       produce(state, (draft) => {
         setCookie("is_login", "success");
         draft.user = action.payload.user;
         draft.is_login = true;
       }),
-    [LOG_OUT]: (state, action) => produce(state, (draft) => {
+    [LOG_OUT]: (state, action) =>
+      produce(state, (draft) => {
         deleteCookie("is_login");
         draft.user = null;
         draft.is_login = false;
-    }),
+      }),
     [GET_USER]: (state, action) => produce(state, (draft) => {}),
   },
   initialState
@@ -51,10 +84,10 @@ export default handleActions(
 
 // action creator export
 const actionCreators = {
-  logIn,
   logOut,
   getUser,
   loginAction,
+  signupFB,
 };
 
 export { actionCreators };
