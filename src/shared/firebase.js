fd3e713f5f3972dafd20932bf2925e93274a9bec
minyoung.lim <minyoung.lim@rubycuve.com>
2021-03-12 02:11:20 +0900

    week3-1

--- src/shared/firebase.js
+++ src/shared/firebase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA0olm-KRlbI4Efq4zi9R1syqDL2xReJsE",
@@ -15,5 +16,6 @@ firebase.initializeApp(firebaseConfig);
 
 const apiKey = firebaseConfig.apiKey;
 const auth = firebase.auth();
+const firestore = firebase.firestore();
 
-export{auth, apiKey};
+export{auth, apiKey, firestore};
